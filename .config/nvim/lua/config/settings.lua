ccebd37fae9104424a98fc926ae986c28390080f
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2025-04-10 18:19:24 +0200

    nvim: set virtual_text diagnostics for nvim 0.11

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -93,3 +93,5 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+vim.diagnostic.config({ virtual_text = true })

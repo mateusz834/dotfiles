4746e1a01cccca595c9aed741aab3d73c1f504a0
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2025-08-27 17:45:05 +0200

    nvim: update nvim-cmp config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -175,6 +175,7 @@ require("lazy").setup({
 			'hrsh7th/cmp-nvim-lsp',
 			'hrsh7th/cmp-buffer',
 			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-cmdline',
 			'hrsh7th/nvim-cmp',
 			'L3MON4D3/LuaSnip',
 			'ray-x/lsp_signature.nvim',
@@ -371,51 +372,35 @@ require("lazy").setup({
 			--	}
 			--}
 
-			-- luasnip setup
-			local luasnip = require('luasnip')
-
-			-- nvim-cmp setup
 			local cmp = require('cmp')
-			cmp.setup {
+			cmp.setup({
 				snippet = {
 					expand = function(args)
-						luasnip.lsp_expand(args.body)
+						vim.snippet.expand(args.body)
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-					['<C-d>'] = cmp.mapping.scroll_docs(-4),
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
 					['<C-Space>'] = cmp.mapping.complete(),
-					['<CR>'] = cmp.mapping.confirm {
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					},
-					['<Tab>'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { 'i', 's' }),
-					['<S-Tab>'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { 'i', 's' }),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }),
 				}),
-				sources = {
+				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' },
 					{ name = 'path' },
-					{ name = 'buffer' }
-				}
-			}
+					{ name = 'buffer' },
+				}),
+			})
+			cmp.setup.cmdline({ '/', '?' }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {{ name = 'buffer' }}
+			})
+			cmp.setup.cmdline(':', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({{ name = 'path' }}, {{ name = 'cmdline' }}),
+				matching = { disallow_symbol_nonprefix_matching = false }
+			})
 		end
 	}
 })
